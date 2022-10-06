44ac235441c6e0be2de45de978fb24cd68cd19b1
RanafaB18 <raziqbaba321@gmail.com>
2022-10-06 23:35:47 +0000

    More malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - Allocates memory
+ * @b : int
+ *
+ * Return : pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1 : first string
+ * @s2 : second string
+ * @n : number of bytes to take from s2
+ * Return: pointer to allocated memory contain
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1Count, s2Count, totalCount, i;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (s1Count = 0; s1[s1Count] != '\0'; s1Count++)
+	{
+		;
+	}
+	for (s2Count = 0; s2[s2Count] != '\0'; s2Count++)
+	{
+		;
+	}
+	if (n >= s2Count)
+	{
+		totalCount = s1Count + s2Count;
+		ptr = malloc(totalCount + 1);
+	}
+	else
+	{
+		totalCount = s1Count + n;
+		ptr = malloc(totalCount + 1);
+	}
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < totalCount; i++)
+	{
+		if (i < s1Count)
+		{
+			ptr[i] = s1[i];
+		}
+		else
+		{
+			ptr[i] = s2[i - s1Count];
+		}
+	}
+	return (ptr);
+}
