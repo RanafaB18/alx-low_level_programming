a8f969fecf02f7a125d2f47f5e643b77ecdaabbf
RanafaB18 <raziqbaba321@gmail.com>
2022-10-21 01:32:22 +0000

    freedom

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,12 +2,12 @@
 /**
  * free_list - frees a linked list
  * @head: first node of list
- * 
  * Return: new node
  */
 void free_list(list_t *head)
 {
 	list_t *temp;
+
 	while ((temp = head) != NULL)
 	{
 		head = head->next;
