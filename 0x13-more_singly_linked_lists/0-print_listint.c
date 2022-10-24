4e3c9de5543846dae5ecf5a34e6f53d311e54963
RanafaB18 <raziqbaba321@gmail.com>
2022-10-24 19:02:35 +0000

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t length;
+	size_t length = 0;
 	const listint_t *temp = h;
 
-	length = 0;
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
 		length++;
 	}
+	h = temp;
 
 	return (length);
 }
