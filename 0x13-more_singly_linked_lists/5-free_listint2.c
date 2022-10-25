065be617f77607fbbe371e31ecc024bf5716d1a3
RanafaB18 <raziqbaba321@gmail.com>
2022-10-25 18:20:15 +0000

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 	listint_t *current;
-
-	temp = *head;
-	while (temp != NULL)
-	{
-		current = temp;
-		temp = temp->next;
-		free(current);
+	if (head != NULL)
+	{	
+		temp = *head;
+		while (temp != NULL)
+		{
+			current = temp;
+			temp = temp->next;
+			free(current);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
