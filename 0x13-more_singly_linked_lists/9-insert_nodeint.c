7e784166bfeb35e7b12bf937258ed0ac6626921e
RanafaB18 <raziqbaba321@gmail.com>
2022-10-25 22:34:12 +0000

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			if (idx == 0)
 			{
+				new_node->n = n;
 				new_node->next = *head;
 				*head = new_node;
 			}
@@ -37,8 +38,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				prev->next = new_node;
 				new_node->n = n;
 				new_node->next = temp;
-				return (new_node);
 			}
+			return (new_node);
+
 		}
 		prev = temp;
 		temp = temp->next;
