d344e2e46039219d37b5d92bcfbc1c6940143896
RanafaB18 <raziqbaba321@gmail.com>
2022-11-01 01:07:20 +0000

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <elf.h>
+#include "main.h"
 
 /**
  * print_addr - prints address
