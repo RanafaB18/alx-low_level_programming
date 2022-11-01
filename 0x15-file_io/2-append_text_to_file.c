6f1a00539d8d0f911212f047825069a31d25d3dd
RanafaB18 <raziqbaba321@gmail.com>
2022-11-01 00:30:21 +0000

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,10 +10,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, i;
 	int bytes;
 
-	if (access(filename, F_OK) == 0)
-	{
-		return (1);
-	}
 	if (filename == NULL)
 	{
 		return (-1);
