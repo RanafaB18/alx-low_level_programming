fd991d9184d2bb81ff02f1c640c6f1be86aa4525
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 15:56:11 +0000

    Ex 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+  * free_dlistint - free the linked list
+  * @head: head of list
+  * Return: THE VOID
+  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current = head;
