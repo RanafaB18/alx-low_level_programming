46b07c06cccd339bd483b4732af84e395d4de186
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 18:40:27 +0000

    Ex 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *newNode = malloc(sizeof(dlistint_t));
 	unsigned int count = 0;
-	
+
 	if (!newNode)
 		return (NULL);
 	newNode->prev = NULL;
