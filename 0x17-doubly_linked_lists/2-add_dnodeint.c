6c26e6d01a7f678a87eae0533dd239fb14908caa
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 15:07:15 +0000

    Ex 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
-  * add_dnodeint - adds node to beginning of list
-  * @h: head of list
-  * @n: new node value
-  * Return: address of new node on success else NULL
-  */
+ * add_dnodeint - adds node to beginning of list
+ * @head: head of list
+ * @n: new node value
+ * Return: address of new node on success else NULL
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-	
+
 	if (!newNode)
 	{
 		return (NULL);
