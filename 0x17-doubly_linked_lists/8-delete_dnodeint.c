ef2a74655647ff9d924d148b665dce00d2f778ca
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 21:13:15 +0000

    Ex 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			*head = NULL;
 		}
-		(*head)->prev = NULL;
+		if (!*head)
+			(*head)->prev = NULL;
 		return (1);
 	}
 	while (temp->next)
