517832d8d633c4bfc8bcf54217a8bf68f9be65e1
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 17:19:05 +0000

    Ex 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = newNode;
 		return (newNode);
 	}
-	while (temp)
+	while (temp->next)
 	{
 		if (count + 1 == idx)
 		{
