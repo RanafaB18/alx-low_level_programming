2b510f31ccb03b03f79cb05f39cbd0d52c2d68f5
RanafaB18 <raziqbaba321@gmail.com>
2022-12-08 16:22:37 +0000

    Ex 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+  * sum_dlistint - get sum of all values in list
+  * @head: head of list
+  * Return: 0 if list is empty else total
+  */
 int sum_dlistint(dlistint_t *head)
 {
 	int total = 0;
