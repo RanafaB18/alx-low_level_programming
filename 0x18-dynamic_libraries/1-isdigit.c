0aada74c72ef8e41868f7004daba91e4feab72aa
RanafaB18 <raziqbaba321@gmail.com>
2022-12-12 19:02:41 +0000

    Ex 0

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* _isdigit - Checks for a digit.
+* @c: input character
+*
+* Return: 1 if c is a digit or else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
