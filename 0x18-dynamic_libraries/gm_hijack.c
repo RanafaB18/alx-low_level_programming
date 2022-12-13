aec8bff8943c597643a0b377eb7f1e4b370fef36
RanafaB18 <raziqbaba321@gmail.com>
2022-12-13 01:23:31 +0000

    Ex 101

--- 0x18-dynamic_libraries/gm_hijack.c
+++ 0x18-dynamic_libraries/gm_hijack.c
@@ -6,43 +6,6 @@
  */
 int rand(void)
 {
-	static int val = -1;
-	int a, m, random, seed = 1234;
-	
-	val++;
-	if (val == 0)
-	{
-		printf("--> ");
-		exit(0);
-	}
-	if (val == 1)
-	{
-		printf("Please ");
-		exit(0);
-	}
-	if (val == 2)
-	{
-		printf("make ");
-		exit(0);
-	}
-	if (val == 3)
-	{
-		printf("me ");
-		exit(0);
-	}
-	if (val == 4)
-	{
-		printf("win");
-		exit(0);
-	}
-	if (val == 5)
-	{
-		printf("!\n");
-		exit (0);
-	}
-	a = 16807;
-	m = 214748;
-	seed = (a * seed) % m;
-	random = seed / m;
-	return (random);
+	printf("--> Please make me win!\n");
+	exit(0);
 }
