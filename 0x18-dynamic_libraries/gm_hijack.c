2de07da56bdfd58a1c9d82dc2152b06f8c648987
RanafaB18 <raziqbaba321@gmail.com>
2022-12-13 00:05:20 +0000

    Ex 101

---
+++ 0x18-dynamic_libraries/gm_hijack.c
@@ -0,0 +1,30 @@
+int rand(void)
+{
+	static int val = -1;
+	val++;
+	if (val == 0)
+	{
+		return (9);
+	}
+	if (val == 1)
+	{
+		return (8);
+	}
+	if (val == 2)
+	{
+		return (10);
+	}
+	if (val == 3)
+	{
+		return (24);
+	}
+	if (val == 4)
+	{
+		return (75);
+	}
+	if (val == 5)
+	{
+		return (9);
+	}
+	return (0);
+}
