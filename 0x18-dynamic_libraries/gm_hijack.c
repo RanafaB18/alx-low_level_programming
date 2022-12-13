04e35aa58c25a64058cf832e0460c865c2b40de0
RanafaB18 <raziqbaba321@gmail.com>
2022-12-13 01:30:11 +0000

    Ex 101

--- 0x18-dynamic_libraries/gm_hijack.c
+++ 0x18-dynamic_libraries/gm_hijack.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * rand - returns each of the clients numbers when called
  * Return: 0
  */
 int rand(void)
 {
-	printf("--> Please make me win!\n");
-	exit(0);
+	static int val = -1;
+
+	val++;
+	if (val == 0)
+		return 8;
+	if (val == 1)
+		return 8;
+	if (val == 2)
+		return 7;
+	if (val == 3)
+		return 9;
+	if (val == 4)
+		return 23;
+	if (val == 5)
+		return 74;
+	return val  % 1234;
 }
