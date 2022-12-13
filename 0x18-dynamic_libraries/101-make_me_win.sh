3e310a3f51edb2c94293cfafa65e6a1f8edec3d7
RanafaB18 <raziqbaba321@gmail.com>
2022-12-13 00:16:09 +0000

    Ex 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/RanafaB18/alx-low_level_programming/raw/main/0x18-dynamic_libraries/gm_hijack.so 
-export LD_PRELOAD=/tmp
+export LD_PRELOAD=/tmp/gm_hijack.so
