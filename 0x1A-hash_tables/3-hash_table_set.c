b9cf29be32be7c6d8331b4b940b0bcd865e67f77
RanafaB18 <raziqbaba321@gmail.com>
2023-01-07 03:38:30 +0000

    Ex 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,9 +7,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-    {
+	{
 		return (0);
-    }
+	}
 	copyOfValue = strdup(value);
 	if (copyOfValue == NULL)
 		return (0);
