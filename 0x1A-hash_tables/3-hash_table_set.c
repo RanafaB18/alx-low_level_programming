58221b394a4fcdedab8be90d73da07ebf0095629
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-07 03:35:56 +0000

    Ex 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,33 +1,28 @@
 #include "hash_tables.h"
-/**
- * hash_table_set - add element to hash table
- * @ht: pointer to hash table
- * @key: key
- * @value: value
- *
- * Return: 1 if successful else 0
- */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newNode;
 	char *copyOfValue;
-	unsigned long int index;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
+    {
 		return (0);
-	}
+    }
 	copyOfValue = strdup(value);
 	if (copyOfValue == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	if (strcmp(ht->array[index]->key, key) == 0)
+	for (i = index; ht->array[i]; i++)
 	{
-		free(ht->array[index]->value);
-		ht->array[index]->value = copyOfValue;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = copyOfValue;
+			return (1);
+		}
 	}
 
 	newNode = malloc(sizeof(hash_node_t));
