f1a24dbe285e772ae0148e3bca853dd9cbb74cdc
RanafaB18 <raziqbaba321@gmail.com>
2023-01-07 03:30:22 +0000

    Ex 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,43 +1,50 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_set - add element to hash table
+ * @ht: pointer to hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if successful else 0
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *newNode;
-    char *copyOfValue;
-    unsigned long int index;
+	hash_node_t *newNode;
+	char *copyOfValue;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-    {
-        return (0);
-    }
-    copyOfValue = strdup(value);
-    if (copyOfValue == NULL)
-        return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	{
+		return (0);
+	}
+	copyOfValue = strdup(value);
+	if (copyOfValue == NULL)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-    if (strcmp(ht->array[index]->key, key) == 0)
-    {
-        free(ht->array[index]->value);
-        ht->array[index]->value = copyOfValue;
-        return (1);
-    }
+	if (strcmp(ht->array[index]->key, key) == 0)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = copyOfValue;
+		return (1);
+	}
 
-    newNode = malloc(sizeof(hash_node_t));
-    if (newNode == NULL)
-    {
-        free(copyOfValue);
-        return (0);
-    }
-    newNode->key = strdup(key);
-    if (newNode->key == NULL)
-    {
-        free(newNode);
-        return (0);
-    }
-    newNode->value = copyOfValue;
-    newNode->next = ht->array[index];
-    ht->array[index] = newNode;
+	newNode = malloc(sizeof(hash_node_t));
+	if (newNode == NULL)
+	{
+		free(copyOfValue);
+		return (0);
+	}
+	newNode->key = strdup(key);
+	if (newNode->key == NULL)
+	{
+		free(newNode);
+		return (0);
+	}
+	newNode->value = copyOfValue;
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
 
-    return (1);
+	return (1);
 }
