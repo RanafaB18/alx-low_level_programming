3786177320b9baf480a3a232909bbc3b8c614713
RanafaB18 <raziqbaba321@gmail.com>
2023-01-07 01:49:24 +0000

    Ex 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,9 +5,11 @@
  * @size: size of array
  *
  * Return: index of array
-*/
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index = hash_djb2(key);
-    return (index);
+	unsigned long int index;
+
+	index = hash_djb2(key);
+	return (index);
 }
