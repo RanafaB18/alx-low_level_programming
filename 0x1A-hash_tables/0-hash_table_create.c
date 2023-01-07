b6d92e69c7324bed77e78bcde73746c4ef19d610
RanafaB18 <raziqbaba321@gmail.com>
2023-01-07 01:35:11 +0000

    Ex 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 	{
 		return (NULL);
 	}
