612ca4850aa1a27de68b4ec0eb60559ca5d855e5
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-07 01:02:39 +0000

    Ex 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,8 +15,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
-	for (i = 0; i < table->size; i++)
+	table->array = malloc(sizeof(hash_node_t) * size);
+    if (table->array == NULL)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
