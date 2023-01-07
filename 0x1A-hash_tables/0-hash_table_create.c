a3a57aa2d5ac2978d72f8a522f5be74b10c070bb
RanafaB18 <raziqbaba321@gmail.com>
2023-01-07 00:49:05 +0000

    Ex 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,22 +3,22 @@
  * hash_table_create - Creates a hash table
  * @size: size of hash table
  * Return: memory address of hash table
-*/
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-    table = malloc(sizeof(hash_table_t));
-    if (!table)
-    {
-        return (NULL);
-    }
-    table->size = size;
-    table->array = calloc(table->size, sizeof(hash_node_t));
-    for (i = 0; i < table->size; i++)
-    {
-        table->array[i] = NULL;
-    }
-    return (table);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t));
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
