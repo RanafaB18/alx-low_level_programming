014963305b3166b0290e7a56f17c7b3ea92eb783
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-01-07 03:28:02 +0000

    Ex 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    hash_node_t *newNode;
+    char *copyOfValue;
+    unsigned long int index;
+
+    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+    {
+        return (0);
+    }
+    copyOfValue = strdup(value);
+    if (copyOfValue == NULL)
+        return (0);
+
+    index = key_index((const unsigned char *)key, ht->size);
+
+    if (strcmp(ht->array[index]->key, key) == 0)
+    {
+        free(ht->array[index]->value);
+        ht->array[index]->value = copyOfValue;
+        return (1);
+    }
+
+    newNode = malloc(sizeof(hash_node_t));
+    if (newNode == NULL)
+    {
+        free(copyOfValue);
+        return (0);
+    }
+    newNode->key = strdup(key);
+    if (newNode->key == NULL)
+    {
+        free(newNode);
+        return (0);
+    }
+    newNode->value = copyOfValue;
+    newNode->next = ht->array[index];
+    ht->array[index] = newNode;
+
+    return (1);
+}
