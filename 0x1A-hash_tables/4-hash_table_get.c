1a1c5182dbf73b1710b473ad9864a8f1183a6cae
RanafaB18 <raziqbaba321@gmail.com>
2023-01-07 03:50:33 +0000

    Ex 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,16 +16,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
-    {
+	{
 		return (NULL);
-    }
+	}
 	node = ht->array[index];
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
 
-    if (node == NULL)
-    {
-        return (NULL);
-    }
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 	return (node->value);
 }
